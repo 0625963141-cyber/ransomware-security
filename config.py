676a1ec9227d48afea02f55517a78168bec13e4c
0625963141-cyber <cs7778503@gmail.com>
2024-06-19 15:44:43 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# Chemin du répertoire à surveiller
+DIRECTORY_TO_WATCH = '/chemin/vers/votre/repertoire'
+
+# Chemin du répertoire de sauvegarde
+BACKUP_DIRECTORY = '/chemin/vers/votre/repertoire_de_sauvegarde'
+
+# Configuration pour l'alerte par email
+EMAIL_CONFIG = {
+    'smtp_server': 'smtp.example.com',
+    'smtp_port': 587,
+    'sender_email': 'your_email@example.com',
+    'receiver_email': 'recipient_email@example.com',
+    'password': 'your_email_password'
+}
+
+# Autres configurations (à adapter selon vos besoins)
