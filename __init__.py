0d8cf4554708d3fe5c91daa9fcfe6106a4f13eca
0625963141-cyber <cs7778503@gmail.com>
2024-06-19 16:16:12 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# __init__.py
+from .backup import BackupManager
+from .system_dection import AlertSystem
+from .ransomware_detection import RansomwareDetection
+from .file_monitor import FileMonitor
